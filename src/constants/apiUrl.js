84783a7a2c27bdec364eddb14b7067e7a3180548
marshallhch <marshallhch@gmail.com>
2024-07-10 12:12:00 +0900

    change server to aws ec2 postgres

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,13 +1,13 @@
 const REST_COUNTRIES_API_URL =
   'https://restcountries.com/v3.1/all?fields=name,flags,languages';
 
-const GET_VISITORS_API_URL = 'http://localhost:3001/visitors';
-const GET_CUSTOMERS_API_URL = 'http://localhost:3001/customers';
-const GET_REVENUE_API_URL = 'http://localhost:3001/revenue';
-const GET_TARGET_REALITY_API_URL = 'http://localhost:3001/targetReality';
-const GET_TOP_PRODUCTS_API_URL = 'http://localhost:3001/topProducts';
-const GET_SALES_MAP_API_URL = 'http://localhost:3001/salesMap';
-const GET_VOLUME_SERVICES_API_URL = 'http://localhost:3001/volumeServices';
+const GET_VISITORS_API_URL = 'http://3.27.35.124/visitors';
+const GET_CUSTOMERS_API_URL = 'http://3.27.35.124/customers';
+const GET_REVENUE_API_URL = 'http://3.27.35.124/revenue';
+const GET_TARGET_REALITY_API_URL = 'http://3.27.35.124/targetReality';
+const GET_TOP_PRODUCTS_API_URL = 'http://3.27.35.124/topProducts';
+const GET_SALES_MAP_API_URL = 'http://3.27.35.124/salesMap';
+const GET_VOLUME_SERVICES_API_URL = 'http://3.27.35.124/volumeServices';
 
 export {
   GET_CUSTOMERS_API_URL,
